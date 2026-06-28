3cf5d4e1db623191a71394f74a8315b034eff252
Sarumathi Gowtham <119105948+nsarumathi@users.noreply.github.com>
2026-06-28 15:44:43 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "shopnow-terraform-state"
+    bucket       = "shopnow-terraform-state1"
     key          = "prod/terraform.tfstate"
     region       = "ap-south-2"
     encrypt      = true
