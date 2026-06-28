63d45fb5ea9fa48458c94c14dbcddca620af6608
nsarumathi <sarumathinallasivam97@gmail.com>
2026-06-28 15:40:34 +0530

    s3 terraform storage added

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "shopnow-terraform-state"
+    key          = "prod/terraform.tfstate"
+    region       = "ap-south-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
